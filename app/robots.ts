a9dc98a5057885518d191cb4a3e3a29b1bc8e7d6
rowgregory <rowgregory@gmail.com>
2024-11-07 13:50:45 -0500

    chore(robots):Move robots file to root

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,13 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*', // Apply to all bots
-      allow: ['/'] // Allow bots to crawl the homepage
-    },
+    rules: [
+      {
+        userAgent: '*', // Apply to all bots
+        allow: ['/'], // Allow bots to crawl the homepage
+        disallow: ['/search?q='] // Disallow search pages (if applicable)
+      }
+    ],
     sitemap: ['https://storyconstruction.vercel.app/sitemap.xml'],
     host: 'https://storyconstruction.vercel.app'
   }
